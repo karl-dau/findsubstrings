eeb25e7b7ccc7ec4d84ab02a8296f674abfc648d
Karl Möller <karl@dau.technology>
2023-07-05 12:32:25 +0200

    init commit + tests

---
+++ substrings/substrings.go
@@ -0,0 +1,47 @@
+package substrings
+
+import (
+	"strings"
+)
+
+// FindIndices returns a slice of 1-based indices where the subtext is found in the textToSearch
+// Matching is case-insensitive, returns nil if no matches are found
+// Matching algorithm runs in O(n*m) time, where n is the length of textToSearch and m is the length of subtext
+func FindIndices(textToSearch, subtext string) []int {
+	if len(subtext) > len(textToSearch) {
+		//  nothing to do, early exit
+		return nil
+	}
+	if len(textToSearch) == 0 {
+		//  nothing to do, early exit
+		return nil
+	}
+	if len(subtext) == 0 {
+		//  nothing to do, early exit
+		return nil
+	}
+	var indices []int // no idea how many indices we'll need
+	// lowercase both textToSearch and subtext
+	textToSearch = strings.ToLower(textToSearch)
+	subtext = strings.ToLower(subtext)
+
+	subTextLen := len(subtext)
+
+	for i := 0; i < len(textToSearch); i++ {
+		// check if the 1st character of the subtext matches the current character
+		if textToSearch[i] == subtext[0] {
+			for j := 0; j < subTextLen; j++ {
+				if textToSearch[i+j] != subtext[j] {
+					// if the characters don't match, stop checking
+					break
+				}
+				if j == subTextLen-1 {
+					// we have reached the end of the substring, so
+					//everything must have matched. Add the index to our indices slice
+					indices = append(indices, i+1) // add one to make result 1-based
+				}
+			}
+		}
+	}
+	return indices
+}
---
+++ substrings/substrings_test.go
@@ -0,0 +1,98 @@
+package substrings
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFindIndices(t *testing.T) {
+	type args struct {
+		textToSearch string
+		subtext      string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "empty textToSearch",
+			args: args{
+				textToSearch: "",
+				subtext:      "foo",
+			},
+			want: nil,
+		},
+		{
+			name: "empty subtext",
+			args: args{
+				textToSearch: "foo",
+				subtext:      "",
+			},
+			want: nil,
+		},
+		{
+			name: "subtext longer than textToSearch",
+			args: args{
+				textToSearch: "foo",
+				subtext:      "foobar",
+			},
+			want: nil,
+		},
+		{
+			name: "subtext not found - z",
+			args: args{
+				textToSearch: "Peter told me that peter the pickle piper piped a pitted pickle before he petered out. Phew!",
+				subtext:      "z",
+			},
+			want: nil,
+		},
+		{
+			name: "subtext not found - Peterz",
+			args: args{
+				textToSearch: "Peter told me that peter the pickle piper piped a pitted pickle before he petered out. Phew!",
+				subtext:      "Peterz",
+			},
+			want: nil,
+		},
+		{
+			name: "subtext found - Peter",
+			args: args{
+				textToSearch: "Peter told me that peter the pickle piper piped a pitted pickle before he petered out. Phew!",
+				subtext:      "Peter",
+			},
+			want: []int{1, 20, 75},
+		},
+		{
+			name: "subtext found - peter",
+			args: args{
+				textToSearch: "Peter told me that peter the pickle piper piped a pitted pickle before he petered out. Phew!",
+				subtext:      "peter",
+			},
+			want: []int{1, 20, 75},
+		},
+		{
+			name: "subtext found - pick",
+			args: args{
+				textToSearch: "Peter told me that peter the pickle piper piped a pitted pickle before he petered out. Phew!",
+				subtext:      "pick",
+			},
+			want: []int{30, 58},
+		},
+		{
+			name: "subtext found - pi",
+			args: args{
+				textToSearch: "Peter told me that peter the pickle piper piped a pitted pickle before he petered out. Phew!",
+				subtext:      "pi",
+			},
+			want: []int{30, 37, 43, 51, 58},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FindIndices(tt.args.textToSearch, tt.args.subtext); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FindIndices() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
